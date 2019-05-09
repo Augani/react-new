fa1a28743923bbf25f74b9a541abaa2632e817f1
Augustus Otu <Augani7@gmail.com>
2019-05-09 11:50:14 +0000

    deploying to heroku

---
+++ build/precache-manifest.170dea47165d4c319c0db0d709df0c0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "124d0b1e01e3143240c72549245644ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac8586870b6c63c99d03",
+    "url": "/static/js/2.187850f2.chunk.js"
+  },
+  {
+    "revision": "9fc8973053cc6af18144",
+    "url": "/static/js/main.9b2cf82e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "92237f69961915fb956ee34a9509a951",
+    "url": "/static/media/undraw_Dest.92237f69.svg"
+  }
+]);
